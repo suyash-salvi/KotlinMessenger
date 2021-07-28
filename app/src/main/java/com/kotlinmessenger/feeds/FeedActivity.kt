4ba7ea9b1466ceace40786b9da64bbb433c1d8e8
Suyash <suyashmessi123@gmail.com>
2021-07-28 20:11:46 +0530

    Stable Talkie: Latest feature:Added a New Message Activity

---
+++ app/src/main/java/com/kotlinmessenger/feeds/FeedActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlinmessenger.feeds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlinmessenger.R
+
+class FeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
